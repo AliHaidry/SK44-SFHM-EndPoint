33803f0a48843cda6b0deb45a0bc5fca52c0f82e
AliHaidry <alihaidry11@gmail.com>
2021-03-18 19:54:52 -0400

    code update with function update

--- HelloMessage/handler.js
+++ HelloMessage/handler.js
@@ -1,15 +1,10 @@
-'use strict';
+"use strict";
 
 module.exports.hello = async (event) => {
   return {
     statusCode: 200,
     body: JSON.stringify(
-      {
-        message: 'Hello from Serverless ! Your function executed successfully! Ali :)',
-        input: event,
-      },
-      null,
-      2
+      "Hello from Serverless ! Your function executed successfully! Ali :)"
     ),
   };
 
